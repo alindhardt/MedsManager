36472063ca6c216ea7b152f60393d93a5085a63e
Aputsiaq Lindhardt <51225@airgreenland.gl>
2023-10-30 20:10:19 -0200

    Add project files.

---
+++ MedsManager/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MedsManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MedsManager/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MedsManager
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
